ef42ff3b7fff5cb73be233810157f372b693d363
ehdrylang <ehdrylang@naver.com>
2020-06-25 14:30:16 +0900

    init project
    프로젝트 기본 세팅, 제한 시간 만료 및 중복 체크는 redis 기능을 이용하는 방식으로 접근

---
+++ src/test/java/com/kakaopay/sprinkler/SprinklerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopay.sprinkler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinklerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
